3630a6b75d7a0c14f98aa9b1a50daeb5162f72d8
aquinoa-nba <aquinoa@student.21-school.ru>
2021-09-08 23:42:43 +0300

    fixed initialization in the node constructor

--- includes/SerDeser.hpp
+++ includes/SerDeser.hpp
@@ -12,10 +12,10 @@ struct node
     BaseHolder            *value;
     std::vector<node*>    branches;    //    tree branches
 /*    CONSTRUCTORS    */
-    node() : value(nullptr) {}                                     //    default constructor
-    node(const int &val) { value = new IntHolder(val); }           //    constructor for IntHolder's node
-    node(const char *val) { value = new CharArrayHolder(val); }    //    constructor for CharArrayHolder's node
-    node(const double &val) { value = new DoubleHolder(val); }     //    constructor for DoubleHolder's node
+    node() : value(nullptr) {}                                    //    default constructor
+    node(const int &val) : value(new IntHolder(val)) {}           //    constructor for IntHolder's node
+    node(const char *val) : value(new CharArrayHolder(val)) {}    //    constructor for CharArrayHolder's node
+    node(const double &val) : value(new DoubleHolder(val)) {}     //    constructor for DoubleHolder's node
 };
 
 node    make_tree();
